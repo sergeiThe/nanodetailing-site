619b43682b63f809964f194e051bf9666bc75229
Sergei Johansen <sergei.johansen.97@gmail.com>
2023-08-22 15:52:08 +0200

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,42 +2,44 @@ import { Footer } from "@/modules/footer";
 import { SideSocialLinks } from "@/modules/side-social-links";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Analytics from "./analytics/Analytics";
 import "./globals.css";
 import { ReduxProvider } from "./provider";
 
 const font = Poppins({
-    subsets: ["latin"],
-    weight: ["100", "400", "700"],
+  subsets: ["latin"],
+  weight: ["100", "400", "700"],
 });
 
 export const metadata: Metadata = {
-    title: "Nanodetailing | Trondheim",
-    description:
-        "Vi i NanoDetailing spesialiserer oss på kosmetisk behandling av bil. Vår målsetning er å tilby deg innsyn i våre tekniske løsninger og kompetanse for at du skal kunne oppnå de beste resultatene så trygt og effektivt som mulig.",
-    keywords: [
-        "nanodetailing",
-        "detailing trondheim",
-        "bilpleie trondheim",
-        "keramisk lakkforsegling trondheim",
-    ],
+  title: "Nanodetailing | Trondheim",
+  description:
+    "Vi i NanoDetailing spesialiserer oss på kosmetisk behandling av bil. Vår målsetning er å tilby deg innsyn i våre tekniske løsninger og kompetanse for at du skal kunne oppnå de beste resultatene så trygt og effektivt som mulig.",
+  keywords: [
+    "nanodetailing",
+    "detailing trondheim",
+    "bilpleie trondheim",
+    "keramisk lakkforsegling trondheim",
+  ],
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="no">
-            <body className={font.className}>
-                <ReduxProvider>
-                    <div id="backdrop"></div>
-                    <div id="modal"></div>
-                    <SideSocialLinks />
-                    {children}
-                </ReduxProvider>
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="no">
+      <body className={font.className}>
+        <Analytics />
+        <ReduxProvider>
+          <div id="backdrop"></div>
+          <div id="modal"></div>
+          <SideSocialLinks />
+          {children}
+        </ReduxProvider>
+        <Footer />
+      </body>
+    </html>
+  );
 }

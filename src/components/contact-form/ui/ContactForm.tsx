0f308c911778b657ace8b7a6e532ab1aa3a2888c
Sergei Johansen <sergei.johansen.97@gmail.com>
2023-08-22 12:55:18 +0200

    contact fix

--- src/components/contact-form/ui/ContactForm.tsx
+++ src/components/contact-form/ui/ContactForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import LinkExternal from "@/components/link-external/LinkExternal";
 import { Button } from "@/shared/ui/button/Button";
 import { Form } from "@/shared/ui/form/Form";
 import { Input } from "@/shared/ui/form/Input";
@@ -7,64 +8,86 @@ import { Textarea } from "@/shared/ui/form/Textarea";
 import emailjs from "@emailjs/browser";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { BsMessenger } from "react-icons/bs";
 
 // TODO: Add contact information
 const ContactForm = () => {
-    const [formSent, setFormSent] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        emailjs
-            .sendForm(
-                "service_te8odrw",
-                "template_mapsgnw",
-                e.target as HTMLFormElement,
-                "xhZxJ0D-bhia6zE1u"
-            )
-            .then((res) => {
-                setFormSent(true);
-                setTimeout(() => {
-                    // redirect
-                    router.push("/");
-                }, 1000);
-            });
-    };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
 
-    return (
-        <div className="py-10">
-            {!formSent && (
-                <Form
-                    variant="primary"
-                    onSubmit={handleSubmit}
-                    className="mx-auto"
-                >
-                    <Label htmlFor="navn" text="Navn" />
-                    <Input id="navn" name="navn" type="text" required />
+    setIsLoading(true);
 
-                    <Label htmlFor="epost" text="E-post" />
-                    <Input id="epost" name="epost" type="email" required />
+    emailjs
+      .sendForm(
+        "service_te8odrw",
+        "template_mapsgnw",
+        e.target as HTMLFormElement,
+        "xhZxJ0D-bhia6zE1u"
+      )
+      .then((res) => {
+        setIsLoading(false);
+        setIsSuccess(true);
+      })
+      .catch((res) => {
+        setIsError(true);
+      });
+  };
 
-                    <Label htmlFor="telefon" text="Telefon" />
-                    <Input id="telefon" name="telefon" type="text" />
+  return (
+    <div className="py-10">
+      {!isSuccess && (
+        <Form variant="primary" onSubmit={handleSubmit} className="mx-auto">
+          <Label htmlFor="navn" text="Navn" />
+          <Input id="navn" name="navn" type="text" required />
 
-                    <Label htmlFor="beskjed" text="Beskjed" />
-                    <Textarea id="beskjed" />
+          <Label htmlFor="epost" text="E-post" />
+          <Input id="epost" name="epost" type="email" required />
 
-                    <Button text="Send" variant="primary" />
-                    <div>Vestre Rosten 102, Trondheim 7075</div>
-                    <div>+47 405 90 005</div>
-                    <div>post@nanodetailing.no</div>
-                </Form>
-            )}
-            {formSent && (
-                <div className="text-center text-8xl py-24 text-red-700">
-                    Takk!
-                </div>
-            )}
+          <Label htmlFor="telefon" text="Telefon" />
+          <Input id="telefon" name="telefon" type="text" />
+
+          <Label htmlFor="beskjed" text="Beskjed" />
+          <Textarea id="beskjed" />
+
+          <Button
+            text={isLoading ? "Sender..." : "Send"}
+            variant="primary"
+            disabled={isLoading}
+          />
+          {isError && (
+            <div className="py-5 flex gap-2">
+              <p className="text-red-500">
+                Beklager, men det har skjedd et eller annet med skemaet at det
+                ikke fungerte som det skulle... Vennligst prøv å nå oss via
+                messenger eller evt. telefon.
+              </p>
+              <span>
+                <LinkExternal
+                  className="text-red-500 hover:text-red-300 cursor-pointer text-5xl"
+                  linkContent={<BsMessenger />}
+                  href="http://m.me/NanoDetailingAS"
+                />
+              </span>
+            </div>
+          )}
+          <div>Vestre Rosten 102, Trondheim 7075</div>
+          <div>+47 405 90 005</div>
+          <div>post@nanodetailing.no</div>
+        </Form>
+      )}
+      {isSuccess && (
+        <div className="text-center text-5xl py-24 text-red-700">
+          Takk! Du hører fra oss snarest mulig
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default ContactForm;
--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,29 +1,36 @@
 import React, { ReactNode } from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    variant: "primary" | "secondary";
-    role?: string;
-    text: string;
-    icon?: ReactNode;
+  variant: "primary" | "secondary";
+  role?: string;
+  text: string;
+  icon?: ReactNode;
+  disabled?: boolean;
 }
 
 const commonStyle =
-    "flex gap-[6px] justify-center items-center text-white pt-2 pb-[10px] pl-6 pr-8  select-none my-2 hover:shadow-lg hover:shadow-slate-500";
+  "flex gap-[6px] justify-center items-center  pt-2 pb-[10px] pl-6 pr-8  select-none my-2 ";
 
 const btnVariants = {
-    primary: "bg-red-700 hover:bg-red-500 active:bg-red-300 ",
-    secondary: "bg-slate-700 hover:bg-slate-500 active:bg-slate-300",
+  primary:
+    "bg-red-700 hover:bg-red-500 active:bg-red-300 text-white hover:shadow-lg hover:shadow-slate-500 ",
+  secondary:
+    "bg-slate-700 hover:bg-slate-500 active:bg-slate-300 text-white hover:shadow-lg hover:shadow-slate-500 ",
+  disabled: "bg-slate-100 text-black",
 };
 
 export const Button = (props: ButtonProps) => {
-    return (
-        <button
-            role={props.role ? props.role : "button"}
-            className={btnVariants[props.variant] + commonStyle}
-            {...props}
-        >
-            <span className="mt-[2px]">{props.icon}</span>
-            {props.text}
-        </button>
-    );
+  return (
+    <button
+      disabled={props.disabled}
+      role={props.role ? props.role : "button"}
+      className={
+        btnVariants[!props.disabled ? props.variant : "disabled"] + commonStyle
+      }
+      {...props}
+    >
+      <span className="mt-[2px]">{props.icon}</span>
+      {props.text}
+    </button>
+  );
 };

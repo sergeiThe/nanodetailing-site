1bcd4820e6d4f0b322776c61e9bd16d734a22f0e
Sergei Johansen <sergei.johansen.97@gmail.com>
2023-09-06 20:23:26 +0200

    text black

--- src/shared/ui/form/Textarea.tsx
+++ src/shared/ui/form/Textarea.tsx
@@ -1,15 +1,15 @@
 interface Props {
-    id: string;
+  id: string;
 }
 
 export const Textarea = (props: Props) => {
-    return (
-        <textarea
-            rows={4}
-            className="px-2 focus:outline-none bg-white border-[2px] rounded-lg pt-2"
-            name={props.id}
-            id={props.id}
-            placeholder="Hva lurer du på?"
-        ></textarea>
-    );
+  return (
+    <textarea
+      rows={4}
+      className="px-2 text-black focus:outline-none bg-white border-[2px] rounded-lg pt-2"
+      name={props.id}
+      id={props.id}
+      placeholder="Hva lurer du på?"
+    ></textarea>
+  );
 };

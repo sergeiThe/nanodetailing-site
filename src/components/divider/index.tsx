5147eee0cd5223b74f89cc015e75b1b35b657106
sergei johansen <sergeitheprogrammer@gmail.com>
2023-10-15 13:04:23 +0200

    add divider

---
+++ src/components/divider/index.tsx
@@ -0,0 +1,9 @@
+const Divider = () => {
+  return (
+    <div className="w-full py-5 flex items-center">
+      <div className="h-1 bg-zinc-300 w-full"></div>
+    </div>
+  )
+}
+
+export default Divider

ce6aba2a8130c060113beccabe4aa999887a42a1
Sergei Johansen <sergeij@stud.ntnu.no>
2024-05-11 11:35:00 +0200

    remove hex link

--- src/components/link-hexagonal/ui/LinkHex.tsx
+++ src/components/link-hexagonal/ui/LinkHex.tsx
@@ -4,46 +4,46 @@ import { HTMLAttributes } from "react";
 import { Link as ScrollLink } from "react-scroll";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
-    linkContent: string;
-    className?: string | undefined;
-    image: string;
-    anchor?: string | null | undefined;
-    pageLink?: string;
+  linkContent: string;
+  className?: string | undefined;
+  image: string;
+  anchor?: string | null | undefined;
+  pageLink?: string;
 }
 
 const LinkHex = (props: Props) => {
-    return (
-        <div onClick={props.onClick} className={`hex-link ${props.className}`}>
-            <div className="hex-link-img">
-                <Image
-                    alt={props.linkContent}
-                    fill
-                    style={{ objectFit: "contain" }}
-                    src={"/images/" + props.image}
-                />
-                {props.anchor && (
-                    <ScrollLink
-                        to={props.anchor}
-                        spy={true}
-                        smooth={true}
-                        offset={50}
-                        duration={500}
-                        className="hex-link-anchor"
-                    >
-                        {props.linkContent}
-                    </ScrollLink>
-                )}
-                {!props.anchor && !props.pageLink && (
-                    <a className="hex-link-anchor">{props.linkContent}</a>
-                )}
-                {props.pageLink && (
-                    <Link className="hex-link-anchor" href={props.pageLink}>
-                        {props.linkContent}
-                    </Link>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div onClick={props.onClick} className={`hex-link ${props.className}`}>
+      <div className="hex-link-img">
+        <Image
+          alt={props.linkContent}
+          fill
+          style={{ objectFit: "contain" }}
+          src={"/images/" + props.image}
+        />
+        {props.anchor && (
+          <ScrollLink
+            to={props.anchor}
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            className="hex-link-anchor"
+          >
+            {props.linkContent}
+          </ScrollLink>
+        )}
+        {!props.anchor && !props.pageLink && (
+          <a className="hex-link-anchor">{props.linkContent}</a>
+        )}
+        {props.pageLink && (
+          <Link className="hex-link-anchor" href={props.pageLink}>
+            {props.linkContent}
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default LinkHex;
--- src/modules/hero-section/ui/HexLinks.tsx
+++ src/modules/hero-section/ui/HexLinks.tsx
@@ -2,36 +2,37 @@ import { LinkHex } from "@/components/link-hexagonal";
 import { useRouter } from "next/navigation";
 
 const HexLinks = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <div className="relative bottom-20 md:translate-x-0 translate-x-[150px]">
-            <LinkHex
-                className="md:top-[-101px] top-[-60px] md:right-[303px] right-[188px]"
-                linkContent="Kontakt"
-                image="tesla2.png"
-                onClick={() => router.push("/kontakt")}
-            />
-            <LinkHex
-                className="md:top-[-101px] top-[-60px] md:right-[101px] right-[61px]"
-                linkContent="Tjenester"
-                image="tesla1.png"
-                anchor="services"
-            />
-            <LinkHex
-                className="md:top-[81px] top-[60px] md:right-[0px] right-[-2px]"
-                linkContent="Faste priser"
-                image="audi.png"
-                anchor="prices"
-            />
-            <LinkHex
-                className="md:top-[81px] top-[60px] md:right-[202px] right-[125px]"
-                linkContent="Hendlex kurs"
-                image="volk1.png"
-                anchor="course"
-            />
-        </div>
-    );
+  return (
+    <div className="relative bottom-20 md:translate-x-0 translate-x-[150px]">
+      <LinkHex
+        className="md:top-[-101px] top-[-60px] md:right-[303px] right-[188px]"
+        linkContent="Kontakt"
+        image="tesla2.png"
+        onClick={() => router.push("/kontakt")}
+      />
+      <LinkHex
+        className="md:top-[-101px] top-[-60px] md:right-[101px] right-[61px]"
+        linkContent="Tjenester"
+        image="tesla1.png"
+        anchor="services"
+      />
+
+      {/*<LinkHex
+        className="md:top-[81px] top-[60px] md:right-[0px] right-[-2px]"
+        linkContent="Faste priser"
+        image="audi.png"
+        anchor="prices"
+      />*/}
+      <LinkHex
+        className="md:top-[81px] top-[60px] md:right-[202px] right-[125px]"
+        linkContent="Hendlex kurs"
+        image="volk1.png"
+        anchor="course"
+      />
+    </div>
+  );
 };
 
 export default HexLinks;

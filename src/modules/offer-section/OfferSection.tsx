36b33ba6836dd2da961b25dedf7c00416373b3d3
Sergei Johansen <sergeij@stud.ntnu.no>
2024-05-11 13:01:20 +0200

    change offer

--- src/modules/offer-section/OfferSection.tsx
+++ src/modules/offer-section/OfferSection.tsx
@@ -1,45 +1,44 @@
-import {Button} from "@/shared/ui/button/Button";
+import { Button } from "@/shared/ui/button/Button";
 import OfferCard from "@/modules/offer-section/components/OfferCard";
-import {useRouter} from "next/navigation";
-import {useAppDispatch} from "@/shared/hooks/useAppDispatch";
-import {open} from "@/shared/model/serviceSlice";
+import { useRouter } from "next/navigation";
+import { useAppDispatch } from "@/shared/hooks/useAppDispatch";
+import { open } from "@/shared/model/serviceSlice";
 
 const OfferSection = () => {
-  const navigate = useRouter()
+  const navigate = useRouter();
   const dispatch = useAppDispatch();
   return (
-    <section className="min-h-[400px] max-w-[1300px]
+    <section
+      className="min-h-[400px] max-w-[1300px]
      shadow-lg
-    mx-auto lg:mt-20 p-10 lg:rounded-2xl">
-      <h2 className="text-center pt-10 pb-20 text-5xl text-zinc-700">Våre tilbud</h2>
+    mx-auto lg:mt-20 p-10 lg:rounded-2xl"
+    >
+      <h2 className="text-center pt-10 pb-20 text-5xl text-zinc-700">
+        Våre tilbud
+      </h2>
       <div className="flex flex-col md:flex-row gap-5 justify-center">
-
-          <OfferCard
-            discount={20}
-            title={"Keramisk coating"}
-            content={"Beskytt bilen og bevar bilens verdi"}
-            buttonCb={() => navigate.push('/coating')}
-          />
+        <OfferCard
+          discount={20}
+          title={"Keramisk coating"}
+          content={"Beskytt bilen og bevar bilens verdi"}
+          buttonCb={() => navigate.push("/coating")}
+        />
         <OfferCard
           warning={"Varer til 1. mai"}
           title={"Solfilm"}
-          list={[
-            "A Stolpe - fra 4200 kr", "B Stolpe - fra 3500 kr"
-          ]}
+          list={["A Stolpe - fra 5900 kr", "B Stolpe - fra 4900 kr"]}
           content={"Beskytt deg selv og dine passasjerer"}
-          buttonCb={() => dispatch(open('solfilm'))}
+          buttonCb={() => dispatch(open("solfilm"))}
         />
         <OfferCard
           discount={15}
           title={"Polering"}
           content={"Gjenoppliv glansen hos Nanodetailing"}
-          buttonCb={() => navigate.push('/polering')}
+          buttonCb={() => navigate.push("/polering")}
         />
-
-
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default OfferSection
+export default OfferSection;
